dfbc416166ea3be33771c7f7986a2921c450cf0f
Matt Hayes <matt@mysterycommand.com>
2017-04-05 20:09:54 -0400

    spike: a test class with array accessors, a backing store, and a generic subtype

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -27,15 +27,75 @@ public class Test : MonoBehaviour {
 		// 	Debug.Log(j + ": " + vector.ToString());
 		// });
 
-		vectors = new List<Vector2>();
+		// vectors = new List<Vector2>();
 
-		EachCell(0, 2, 0, 2, (int col, int row) => {
-			vectors.Add(new Vector2(col, row));
+		// EachCell(0, 2, 0, 2, (int col, int row) => {
+		// 	vectors.Add(new Vector2(col, row));
+		// });
+
+		// Each((Vector2 vector, int i) => {
+		// 	Debug.Log(i + ": " + vector.ToString());
+		// });
+
+		MapTest<Vector2> map = new MapTest<Vector2>(16, 9);
+		map.EachCell((int x, int y) => {
+			map[x, y] = new Vector2(x, y);
 		});
 
-		Each((Vector2 vector, int i) => {
-			Debug.Log(i + ": " + vector.ToString());
-		});
+		for (int x = 0; x < 16; ++x)
+		{
+			for (int y = 0; y < 9; ++y)
+			{
+				Debug.Log(x + ":" + y + " " + map[x, y]);
+			}
+		}
+	}
+
+	public class MapTest<T> where T : new() {
+		private List<T> cells = new List<T>();
+
+		private int width;
+		private int height;
+
+		public MapTest(int width, int height)
+		{
+			this.width = width;
+			this.height = height;
+
+			EachCell((int x, int y) => {
+				cells.Add(new T());
+			});
+		}
+
+		public T this[int x, int y]
+		{
+			get
+			{
+				return cells[x * height + y];
+			}
+
+			set
+			{
+				cells[x * height + y] = value;
+			}
+		}
+
+		public void EachCell(Action<int, int> action = null)
+		{
+			if (action == null)
+			{
+				// if you don't pass in a function it's a noop
+				action = (int x, int y) => {};
+			}
+
+			for (int x = 0; x < width; ++x)
+			{
+				for (int y = 0; y < height; ++y)
+				{
+					action(x, y);
+				}
+			}
+		}
 	}
 
 	public void Each(Action<Vector2, int> action) {
